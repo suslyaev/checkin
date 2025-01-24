cebb1c31907432ac99bf9df6d7c0d12e7d88d406
sdavidk <sdavidk99@icloud.com>
2025-01-24 21:46:23 +0300

    temp update permissions for api

--- config/api/api.py
+++ config/api/api.py
@@ -3,12 +3,14 @@ from rest_framework import viewsets, permissions
 from django.utils.timezone import now
 from .serializers import ModuleInstanceSerializer, ActionSerializer, ContactSerializer
 
+
 class ModuleInstanceViewSet(viewsets.ModelViewSet):
     queryset = ModuleInstance.objects.filter(date_start__gte=now()) | ModuleInstance.objects.filter(date_end__gte=now())
     serializer_class = ModuleInstanceSerializer
     permission_classes = [
-        permissions.DjangoModelPermissions
+        permissions.AllowAny
     ]
+
     def get_queryset(self):
         queryset = ModuleInstance.objects.filter(date_start__gte=now()) | ModuleInstance.objects.filter(date_end__gte=now())
         id_portal = self.request.query_params.get('id', None)
@@ -16,11 +18,12 @@ class ModuleInstanceViewSet(viewsets.ModelViewSet):
             queryset = ModuleInstance.objects.filter(id=int(id_portal))
         return queryset[:50]
 
+
 class ActionSerializerViewSet(viewsets.ModelViewSet):
     queryset = Action.objects.filter(is_last_state=True)
     serializer_class = ActionSerializer
     permission_classes = [
-        permissions.DjangoModelPermissions
+        permissions.AllowAny
     ]
 
     def get_queryset(self):
@@ -35,15 +38,15 @@ class ActionSerializerViewSet(viewsets.ModelViewSet):
         if module_instance_portal is not None:
             queryset = queryset.filter(module_instance=int(module_instance_portal))
         return queryset[:50]
-    
+
 
 class ContactSerializerViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
     permission_classes = [
-        permissions.DjangoModelPermissions
+        permissions.AllowAny
     ]
-    
+
     def get_queryset(self):
         queryset = Contact.objects.all()
         user_id_portal = self.request.query_params.get('id', None)
